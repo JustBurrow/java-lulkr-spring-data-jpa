12ca4ebbe3a85a4fd1311f747b8b0105717591dd
Just Burrow <just.burrow@lul.kr>
2016-09-04 00:17:22 +0900

    #3: 도메인 엔티티의 기본형.
    Task-Url: https://github.com/JustBurrow/spring-data-jpa/issues/3
    
    `int`형 ID를 가지는 도메인 엔티티.

---
+++ src/main/java/kr/lul/spring/data/jpa/common/mapping/IdMapping.java
@@ -0,0 +1,38 @@
+/**
+ * @see https://github.com/JustBurrow/spring-data-jpa
+ */
+package kr.lul.spring.data.jpa.common.mapping;
+
+import kr.lul.data.IntIdDomainObject;
+import kr.lul.data.LongIdDomainObject;
+import kr.lul.data.ObjectIdDomainObject;
+import kr.lul.spring.data.jpa.timestamp.mapping.UpdatableMapping;
+
+/**
+ * {@link IntIdDomainObject}, {@link LongIdDomainObject}, {@link ObjectIdDomainObject}를 위한 기본 JPA 엔티티 매핑.
+ * 복합 키를 가지는 {@link ObjectIdDomainObject}는 사용할 수 없다.
+ *
+ * @author Just Burrow
+ * @since 2016. 9. 3.
+ */
+public abstract class IdMapping {
+  public static abstract class IdEntity extends UpdatableMapping.UpdatableEntity {
+    public static final String ID = "id";
+
+    public IdEntity() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  public static abstract class IdTable extends UpdatableMapping.UpdatableTable {
+    public static final String PK = "pk";
+
+    public IdTable() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  public IdMapping() {
+    throw new UnsupportedOperationException();
+  }
+}
